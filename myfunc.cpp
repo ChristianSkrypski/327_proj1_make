982ecf62d6f8a18804ec41467a086ecbdf2532be
ChristianSkrypski <89998561+ChristianSkrypski@users.noreply.github.com>
2021-09-07 11:43:22 -0400

    Add files via upload

---
+++ myfunc.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include "myfunc.h"
+std::string func()
+{
+	//say hello
+	return "hello";
+}
---
+++ myfunc.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#ifndef MYFUNCTIONSDECLARATIONS_H_
+#define MYFUNCTIONSDECLARATIONS_H_
+	std::string func();
+
+#endif /* MYFUNCTIONSDECLARATIONS_H_ */
